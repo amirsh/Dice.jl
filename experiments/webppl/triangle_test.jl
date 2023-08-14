9a1291806c59be01e44938cb406ab2890afd2596
Amir Shaikhha <amir.shaikhha@ed.ac.uk>
2023-08-14 23:23:38 +0100

    .

---
+++ experiments/webppl/triangle_test.jl
@@ -0,0 +1,68 @@
+
+# localARGS = ARGS
+# n = parse(Int64, localARGS[1])
+n = 101
+nover2 = 52
+
+a = [1/n for _ in 1:n]
+b = [1/n for _ in 1:n]
+c = [1/n for _ in 1:n]
+
+half = [(i == nover2) ? 1.0 : 0.0 for i in 1:n]
+
+less_pr(a :: Vector{Float64}, b :: Vector{Float64}) = begin
+	res = [0.0, 0.0]
+	for i in 1:length(a)
+		for j in 1:length(b)
+			v = (i < j) ? 2 : 1
+			res[v] += a[i] * b[j]
+		end
+	end
+	return res
+end
+
+add_pr(a :: Vector{Float64}, b :: Vector{Float64}) = begin
+	N = length(a) + length(b) - 1
+	res = [0.0 for _ in 1:N]
+	for i in 1:length(a)
+		ip = i - 1
+		for j in 1:length(b)
+			jp = j - 1
+			v = ip + jp + 1
+			res[v] += a[i] * b[j]
+		end
+	end
+	return res
+end
+
+or_pr(a :: Vector{Float64}, b :: Vector{Float64}) = begin
+	res = [0.0, 0.0]
+	res[1] += a[1] * b[1]
+	res[2] += a[1] * b[2]
+	res[2] += a[2] * b[1]
+	res[2] += a[2] * b[2]
+	return res
+end
+
+t1 = less_pr(a, half)
+t2 = less_pr(b, half)
+t3 = less_pr(c, half)
+t4 = or_pr(t1, t2)
+t5 = or_pr(t4, t3)
+println(t5)
+ab = add_pr(a, b)
+ac = add_pr(a, c)
+bc = add_pr(b, c)
+ah = add_pr(a, half)
+bh = add_pr(b, half)
+ch = add_pr(c, half)
+t11 = less_pr(ab, ch)
+t12 = less_pr(ac, bh)
+t13 = less_pr(bc, ah)
+t14 = or_pr(t11, t12)
+t15 = or_pr(t14, t13)
+t20 = or_pr(t5, t15)
+println(t11) # check3_1
+println(t14) # check3_2 # wrong
+println(t15)
+println(t20)
